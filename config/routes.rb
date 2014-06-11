9fa645cab9f0302a442597794513a8d48ed48ec9
Jason <jsadler1488@yahoo.com>
2014-06-10 23:10:41 -0400

    Added Providers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :providers
+  #devise_for :providers
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :articles do
     resources :comments
