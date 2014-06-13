69e76456adf625e40c2401dbef5d4c2b7292c940
def link_to <jsadler1488@yahoo.com>
2014-06-13 14:34:27 -0400

    updated migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140612162501) do
+ActiveRecord::Schema.define(version: 20140613181042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,9 @@ ActiveRecord::Schema.define(version: 20140612162501) do
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
 
-  create_table "provider_registrations", force: true do |t|
+  create_table "mains", force: true do |t|
+    t.string   "title"
+    t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
