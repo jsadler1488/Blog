19b8dfc06ed9a94363d0ee6b6db50c3507f7ecf2
def link_to <jsadler1488@yahoo.com>
2014-06-16 16:30:13 -0400

    added styling. Removed Welcome view and replaced with Main

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
-  root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
