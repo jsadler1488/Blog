4a0ad155e76b13c17c8ffb6397af487bd784f147
Jason <jsadler1488@yahoo.com>
2014-07-12 21:59:57 -0400

    Updated validation order and privatized unnessecarily public methods.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,23 +1,25 @@
 class Client < ActiveRecord::Base
-  include Humanizer
   has_many :reviews, dependent: :destroy
-  attr_accessor :bypass_humanizer
-  require_human_on :create, unless: :bypass_humanizer
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
   validates :first_name, presence: true, length: {minimum:2, maximum: 50}
   validates :last_name, presence: true, length: {minimum:2, maximum: 50}
   validates :birthdate, presence: true
-  validates :zip, presence: true, length: {is:5}
-  validates :address1, presence: true
-  #validates :city, presence: true
-  validates :state, presence: true
-  validate :validate_phone, on: [:create, :update] #:phone, presence: true, length: {is: 10, wrong_length: "Phone Number must have 3 digit area code followed by 7 digit number"}
   validate :dob_check, on: :create
-  validates_acceptance_of :terms_of_service, acceptance: true
+  validates :address1, presence: true
+  validates :city, presence: true
   validates_format_of :city, with: ->(client) {/\A[a-zA-Z ]*\z/i}
+  validates :state, presence: true
+  validates :zip, presence: true, length: {is:5}
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  validate :validate_phone, on: [:create, :update] #:phone, presence: true, length: {is: 10, wrong_length: "Phone Number must have 3 digit area code followed by 7 digit number"}
+  validates_acceptance_of :terms_of_service, acceptance: true
+  include Humanizer
+  attr_accessor :bypass_humanizer
+  require_human_on :create, unless: :bypass_humanizer
   include ActionView::Helpers::NumberHelper
   before_save :run_formatters
+
+  private
   def run_formatters
     self.first_name = self.first_name.titleize
     self.last_name = self.last_name.titleize
@@ -42,7 +44,6 @@ class Client < ActiveRecord::Base
       end
     end
   end
-  private
 
   def dob_check
     if birthdate > 18.years.ago.to_date
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,26 +1,37 @@
 class Provider < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
-  include Humanizer
-  attr_accessor :bypass_humanizer
-  require_human_on :create, unless: :bypass_humanizer
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
   validates :first_name, presence: true, length: {minimum:2, maximum: 50}
   validates :last_name, presence: true, length: {minimum:2, maximum: 50}
   validates :birthdate, presence: true
-  validates :zip, presence: true, length: {is:5}
+  validate :dob_check, on: :create
   validates :address1, presence: true
   validates :city, presence: true
+  validates_format_of :city, with: ->(provider) {/\A[a-zA-Z ]*\z/i}
   validates :state, presence: true
-  validates :phone, presence: true
-  validate :dob_check, on: :create
+  validates :zip, presence: true, length: {is:5}
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  validate :validate_phone, on: [:create, :update]
   validates :filename, allow_blank: true, format:
                             { with: %r{\.gif|jpg|png}i, message: 'incorrect format'}
-  validates_acceptance_of :terms_of_service, acceptance: true
-  validates_format_of :city, with: ->(provider) {/\A[a-zA-Z ]*\z/i}
   validates_size_of :profile_image, maximum: 2.megabytes, message: "too large. Please limit to 2MB"
+  validates_acceptance_of :terms_of_service, acceptance: true
+  include Humanizer
+  attr_accessor :bypass_humanizer
+  require_human_on :create, unless: :bypass_humanizer
   include ActionView::Helpers::NumberHelper
   before_save :run_formatters
+  def profile_img=(input_data)
+    self.filename = input_data.original_filename
+    self.content_type = input_data.content_type.chomp
+    self.profile_image = input_data.read
+  #if self.profile_image.empty?
+  #self.profile_image =
+  #end
+  end
+
+  private
+
   def run_formatters
     self.first_name = self.first_name.titleize
     self.last_name = self.last_name.titleize
@@ -30,19 +41,25 @@ class Provider < ActiveRecord::Base
     self.state = self.state.titleize
   end
 
-  def profile_img=(input_data)
-    self.filename = input_data.original_filename
-    self.content_type = input_data.content_type.chomp
-    self.profile_image = input_data.read
-    #if self.profile_image.empty?
-      #self.profile_image = 
-    #end
-  end
-  private
-
   def dob_check
     if birthdate > 18.years.ago.to_date
       errors[:base] << "Must be 18 years of age to create an account. Those under 18 must have parent or guardian assistance."
+    end
+  end
+
+  def validate_phone
+    if phone.gsub(/\D/, "").length != 10
+      errors[:base] << "Phone Number must have 3 digit area code followed by 7 digit number"
+    else
+      self.phone = number_to_phone(phone, area_code: true)
+    end
+
+    if !alt_phone.blank?
+      if alt_phone.gsub(/\D/, "").length != 10
+        errors[:base] << "Alternate Phone Number must have 3 digit area code followed by 7 digit number"
+      else
+        self.alt_phone = number_to_phone(alt_phone, area_code: true)
+      end
     end
   end
 end
